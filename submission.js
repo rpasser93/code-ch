5a2de5064b8c8db8b4e5fd0ba99d58a790a7623e
rpasser93 <44841501+rpasser93@users.noreply.github.com>
2021-04-14 15:49:05 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -61,5 +61,19 @@ return arrayForStr.join('');
 
 //6
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+let scoreRef = {
+  'a':1, 'e':1, 'i': 1, 'o':1, 'u':1, 'l':1, 'n':1, 'r':1, 's':1, 't':1,
+  'd':2, 'g':2,
+  'b':3, 'c':3, 'm':3, 'p':3,
+  'f':4, 'h':4, 'v':4, 'w':4, 'y':4,
+  'k':5,
+  'j':8, 'x':8,
+  'q':10, 'z':10
+};
+let arrFromString = str.split('');
+let summer = 0;
+for(let itt = 0; itt < arrFromString.length; itt++){
+  summer = summer + scoreRef[arrFromString[itt]];
+}
+return summer;
 };
